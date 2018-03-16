7ebce20d0525131d163949f33b9d8d13384bf9b9
Thomas Gaudin <tgaudin@vekia.fr>
2018-03-16 10:30:07 +0100

    Install & configure babel

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,19 @@ module.exports = {
     library: 'browser',
     libraryTarget: 'umd',
     umdNamedDefine: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
   }
 };
